ba0dbb7751c8168c85d295ea83e4606266318ab3
Araba Taylor <araba.taylor20@gmail.com>
2017-10-01 18:27:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ def assign_rooms(array)
   array.each_with_index.collect {|name, index| "Hello, #{name}! You'll be assigned to room #{index+1}!"}
 end
 
-def printer
-  batch_badge_creator().each {|badge| puts "#{badge}"}
+def printer(array)
+  batch_badge_creator(array).each {|badge| puts "#{badge}"}
   assign_rooms(array).each {|room_assignments| puts "#{room_assignments}"}
 end
