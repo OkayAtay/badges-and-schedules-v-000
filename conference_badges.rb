eac496275c66ac52a339ffb70745d43a430d0248
Araba Taylor <araba.taylor20@gmail.com>
2017-10-01 18:11:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+   "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.collect {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(array)
+  index=1
+  array.each_with_index.collect {|name, index| "Hello, #{name}! You'll be assigned to room #{index}!"}
+end
+
+def printer
+  batch_badge_creator
+end
